9f6c7135217604f1e267ec7801ea3471725ba607
eenees <soeteniels1803@gmail.com>
2024-12-03 15:56:59 +0100

    setup a database connection from go

--- src/main.go
+++ src/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/eenees/scwn-backend/src/controllers"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	sqluser := os.Getenv("MYSQL_USER")
+	sqlPassword := os.Getenv("MYSQL_PASSWORD")
+	sqlDB := os.Getenv("MYSQL_DATABASE")
+
+	sqlConnection := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s", sqluser, sqlPassword, sqlDB)
+
+	fmt.Println(sqlConnection)
+
+	db, err := sql.Open("mysql", sqlConnection)
+	if err != nil {
+		log.Fatal("Error opening database connection: ", err)
+	}
+	defer db.Close()
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
 	router := gin.Default()
+
 	router.GET("/", getAPIInfo)
 	router.GET("/projects", controllers.GetAllProjects)
 	router.GET("/projects/:id", controllers.GetProject)
 	router.POST("/projects", controllers.CreateProject)
+
 	router.Run("localhost:3000")
 }
 
@@ -21,3 +53,4 @@ func getAPIInfo(c *gin.Context) {
 		"version": "v1.0.0",
 	})
 }
+
--- src/models/project.go
+++ src/models/project.go
@@ -6,3 +6,15 @@ type Project struct {
 	Id   uuid.UUID `json:"id,omitempty"`
 	Name string    `json:"name"`
 }
+
+func GetAllProjects() []Project {
+	return []Project{}
+}
+
+func GetProject(id uuid.UUID) uuid.UUID {
+	return id
+}
+
+func CreateProject(project Project) {
+
+}
