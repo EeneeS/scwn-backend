85008461a32f6fba06599d8bd4cd78e06ae2da2a
eenees <soeteniels1803@gmail.com>
2024-12-03 17:47:01 +0100

    refactor

--- src/controllers/projectController.go
+++ src/controllers/projectController.go
@@ -7,20 +7,15 @@ import (
 	"net/http"
 )
 
-var projects = []models.Project{
-	{Id: uuid.New(), Name: "project 1"},
-	{Id: uuid.New(), Name: "project 2"},
-}
-
 func GetAllProjects(c *gin.Context) {
-	projectsDB, err := models.GetAllProjects()
+	projects, err := models.GetAllProjects()
 
 	if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.IndentedJSON(http.StatusOK, projectsDB)
+	c.IndentedJSON(http.StatusOK, projects)
 }
 
 func GetProject(c *gin.Context) {
@@ -31,14 +26,14 @@ func GetProject(c *gin.Context) {
 		return
 	}
 
-	projectDB, err := models.GetProject(id)
+	project, err := models.GetProject(id)
 
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, projectDB)
+	c.JSON(http.StatusOK, project)
 }
 
 func CreateProject(c *gin.Context) {
