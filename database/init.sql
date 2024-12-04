8cb3f4c0824939dde084cc385b7300daf8b1d327
eenees <soeteniels1803@gmail.com>
2024-12-04 12:02:46 +0100

    add firebase files

--- database/init.sql
+++ database/init.sql
@@ -1,13 +1,20 @@
+create table if not exists users (
+  id char(36) PRIMARY KEY,
+  name varchar(255),
+  created_at datetime
+);
+
 create table if not exists projects (
-        id char(36) primary key,
-        name varchar(255),
-        user_id char(36)
+  id char(36) primary key,
+  name varchar(255),
+  user_id char(36),
+  foreign key (user_id) references users(id)
 );
 
 create table if not exists publishTargets (
-        id int auto_increment primary key,
-        project_id char(36),
-        platform varchar(255),
-        url varchar(255),
-        foreign key (project_id) references projects(id)
-)
+  id int auto_increment primary key,
+  project_id char(36),
+  platform varchar(255),
+  url varchar(255),
+  foreign key (project_id) references projects(id)
+);
